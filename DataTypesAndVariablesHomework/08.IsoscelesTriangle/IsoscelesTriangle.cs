e2f89131cc473115b9f159d3eefd31bc0f2377a7
Васил Динев <vassildinev@gmail.com>
2015-01-14 22:55:09 +0200

    Added DataTypesAndVariablesHomework.

---
+++ DataTypesAndVariablesHomework/08.IsoscelesTriangle/IsoscelesTriangle.cs
@@ -0,0 +1,21 @@
+﻿//Write a program that prints an isosceles triangle of 9 copyright symbols ©.
+using System;
+using System.Text;
+class IsoscelesTriangle
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        int level = 5;
+        Console.WriteLine(new string(' ', (level - 1)) + "©");
+        for (int i = 2; i < level; i++)
+        {
+            Console.WriteLine(new string(' ', (level - i)) + "©" + new string(' ', (2 * i - 3)) + "©");
+        }
+        for (int i = 1; i <= level; i++)
+        {
+            Console.Write("©" + new string(' ', 1));
+        }
+        Console.WriteLine();
+    }
+}
---
+++ DataTypesAndVariablesHomework/13.ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,13 @@
+﻿//Write a program that safely compares floating-point numbers (double) with precision eps = 0.000001.
+using System;
+class ComparingFloats
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double eps = 1E-6;
+        Console.WriteLine("Approximately equal (precision 1E-6)?");
+        Console.WriteLine(Math.Abs(a - b) < eps);
+    }
+}
