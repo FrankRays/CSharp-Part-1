19ade47b934863380f9e7c88a222abc9eb40da6a
Васил Динев <vassildinev@gmail.com>
2015-01-20 11:29:39 +0200

    Corrected name.

---
+++ ConsoleInputOutputHomework/10.FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,22 @@
+﻿//Write a program that reads a number n and prints on the console 
+//the first n members of the Fibonacci sequence (at a single line, separated by comma and space -, )
+
+using System;
+class FibonacciNumbers
+{
+    static void Main()
+    {
+        int firstMember = 0;
+        int secondMember = 1;
+        int temporary;
+        int nthMember = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < nthMember; i++)
+        {
+            temporary = firstMember;
+            Console.Write("{0}, ", firstMember);
+            firstMember = secondMember;
+            secondMember += temporary;
+        }
+        Console.WriteLine();
+    }
+}
