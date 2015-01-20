089c477554d01a467febba4ff872ec7b090c6e9a
Васил Динев <vassildinev@gmail.com>
2015-01-20 10:41:17 +0200

    Made some minor corrections to BankAccountData.cs

--- DataTypesAndVariablesHomework/11.BankAccountData/BankAccountData.cs
+++ DataTypesAndVariablesHomework/11.BankAccountData/BankAccountData.cs
@@ -11,10 +11,16 @@ class BankAccountData
         string middleName;
         string lastName;
         string IBAN;
-        decimal balance; //Type is 'decimal' to prevent mistakes when rounding off.
+
+        //Type is 'decimal' to prevent mistakes when rounding off.
+        decimal balance;
+
         string bankName;
-        string firstCreditCard;
-        string secondCreditCard;
-        string thirdCreditCard;
+
+        //According to Wikipedia, a standard banking and financial
+        //credit card has 16 digits and begins either with the digit 4 or 5.
+        long firstCreditCard;
+        long secondCreditCard;
+        long thirdCreditCard;
     }
 }
