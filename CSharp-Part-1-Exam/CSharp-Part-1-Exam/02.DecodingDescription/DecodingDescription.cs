16b00b22a4e6c4fb13d83c34adac5bbd7f2a59ee
Васил Динев <vassildinev@gmail.com>
2015-02-04 00:41:28 +0200

    Added solutions to exam 03 FEB 2015

---
+++ CSharp-Part-1-Exam/CSharp-Part-1-Exam/02.DecodingDescription/DecodingDescription.cs
@@ -0,0 +1,54 @@
+﻿using System;
+class DecodingDescription
+{
+    static void Main()
+    {
+        //INPUT
+        int salt = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+
+        //OUTPUT
+        for (int i = 0; i < text.Length; i++)
+        {
+            char symbol = text[i];
+            if (symbol == '@')
+            {
+                break;
+            }
+            if (char.IsLetter(symbol))
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0:F2}", ((decimal)symbol * salt + 1000) / 100);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", ((int)symbol * salt + 1000) * 100);
+                }
+            }
+            else if (char.IsDigit(symbol))
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0:F2}", ((decimal)symbol + salt + 500) / 100);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", ((long)symbol + salt + 500) * 100);
+                }
+            }
+            else if (!char.IsDigit(symbol) && !char.IsLetter(symbol))
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0:F2}", ((decimal)symbol - salt) / 100);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", ((long)symbol - salt) * 100);
+                }
+            }
+        }
+    }
+}
+
